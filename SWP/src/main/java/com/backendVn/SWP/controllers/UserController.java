0269bc845f133894bd8da7a8e11134ee9d705379
Nguyen Khoa <khoa060704@gmail.com>
2024-05-28 13:14:16 +0700

    basic CRUD user

--- SWP/src/main/java/com/backendVn/SWP/controllers/UserController.java
+++ SWP/src/main/java/com/backendVn/SWP/controllers/UserController.java
@@ -2,10 +2,9 @@ package com.backendVn.SWP.controllers;
 
 import com.backendVn.SWP.entities.User;
 import com.backendVn.SWP.repositories.UserRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.backendVn.SWP.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -13,14 +12,32 @@ import java.util.List;
 @RequestMapping("/user")
 public class UserController {
 
-    private final UserRepository userRepository;
+    @Autowired
+    private UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
     }
 
-    @GetMapping("/find/{name}")
-    public List<User> findUserByName(@PathVariable String name) {
-        return userRepository.findByUserName(name);
+    @GetMapping("/{id}")
+    public User getUserById(@PathVariable String id) {
+        return userService.getUserById(id);
+    }
+
+    @PostMapping
+    public User createUser(@RequestBody User user) {
+        return userService.saveUser(user);
+    }
+
+    @PutMapping("/{id}")
+    public User updateUser(@PathVariable String id, @RequestBody User user) {
+        user.setId(id);
+        return userService.saveUser(user);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteUser(@PathVariable String id) {
+        userService.deleteUser(id);
     }
 }
