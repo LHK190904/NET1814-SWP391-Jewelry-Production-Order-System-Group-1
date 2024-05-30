edadb00b1417ac65f1170e0fac1f9373c1ae46e9
LeNguyenChiBao <baolncse183721@fpt.edu.vn>
2024-05-30 07:20:28 +0700

    Navigator and Footer modification

--- src/component/Footer.jsx
+++ src/component/Footer.jsx
@@ -1,32 +1,32 @@
 import React from "react";
 function Footer() {
-    return (
-        <footer className="bg-black text-center py-4" id="footer">
-            <div className="container-fluid text-start">
-                <div className="row" id="footer-details">
-                    <div className="col-lg-3 offset-lg-2">
-                        <h5>Chi nhánh</h5>
-                        <div>Quận 3</div>
-                        <div>Quận 12</div>
-                        <div>Biên Hòa</div>
-                        <div>Phú Nhuận</div>
-                        <div>Bình Thạnh</div>
-                    </div>
-                    <div className="col-lg-4">
-                        <h5>Phương thức thanh toán</h5>
-                        <div>Mastercard</div>
-                        <div>Visa</div>
-                        <div>Momo</div>
-                    </div>
-                    <div className="col-lg-3">
-                        <h5>Kết nối với chúng tôi</h5>
-                        <div>Facebook</div>
-                        <div>Instagram</div>
-                        <div>Zalo</div>
-                    </div>
-                </div>
-            </div>
-        </footer>
-    )
+  return (
+    <footer className="bg-black text-center py-4" id="footer">
+      <div className="container-fluid text-start">
+        <div className="row" id="footer-details">
+          <div className="col-lg-3 offset-lg-2">
+            <h5>Chi nhánh</h5>
+            <div>Quận 3</div>
+            <div>Quận 12</div>
+            <div>Biên Hòa</div>
+            <div>Phú Nhuận</div>
+            <div>Bình Thạnh</div>
+          </div>
+          <div className="col-lg-4">
+            <h5>Phương thức thanh toán</h5>
+            <div>Mastercard</div>
+            <div>Visa</div>
+            <div>Momo</div>
+          </div>
+          <div className="col-lg-3">
+            <h5>Kết nối với chúng tôi</h5>
+            <div>Facebook</div>
+            <div>Instagram</div>
+            <div>Zalo</div>
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 }
-export default Footer
+export default Footer;
--- src/component/Navigator.jsx
+++ src/component/Navigator.jsx
@@ -1,36 +1,60 @@
 import React from "react";
-import 'bootstrap-icons/font/bootstrap-icons.css';
+import "bootstrap-icons/font/bootstrap-icons.css";
 
 function Navigator() {
-    return (
-        <div className="container-fluid bg-black">
-            <div className="row">
-                <a className="navbar-brand col-lg-2 offset-lg-1" href="#Home">
-                    <img src="./assets/images/logo.jpg" className="img-thumbnail" alt="Logo" />
-                </a>
-                <div className="col-lg-2"></div>
-                <form className="form-inline mx-auto search-bar col-lg-3 align-self-center">
-                    <input className="form-control mr-sm-2" type="search" placeholder="Tìm kiếm..." aria-label="Search" />
-                </form>
-                <div className="col-lg-3 align-self-center" id="icons">
-                    <i className="bi bi-facebook social-icons" />
-                    <i className="bi bi-instagram social-icons" />
-                    <a href="#Cart">GIỎ HÀNG<i className="bi bi-cart3 social-icons" /></a>
-                </div>
-            </div>
-            <div className="row bg-dark">
-                <nav>
-                    <ul>
-                        <li><a href="#Home">TRANG CHỦ</a></li>
-                        <li><a href="#About">THÔNG TIN</a></li>
-                        <li><a href="#Login">THIẾT KẾ</a></li>
-                        <li><a href="#Collection">BỘ SƯU TẬP</a></li>
-                        <li><a href="#Blog">BLOG</a></li>
-                        <li><a href="#Login">ĐĂNG NHẬP</a></li>
-                    </ul>
-                </nav>
-            </div>
+  return (
+    <div className="container-fluid bg-black">
+      <div className="row">
+        <a className="navbar-brand col-lg-2 offset-lg-1" href="#Home">
+          <img
+            src="./assets/images/logo.jpg"
+            className="img-thumbnail"
+            alt="Logo"
+          />
+        </a>
+        <div className="col-lg-2"></div>
+        <form className="form-inline mx-auto search-bar col-lg-3 align-self-center">
+          <input
+            className="form-control mr-sm-2 "
+            type="search"
+            placeholder="Tìm kiếm..."
+            aria-label="Search"
+          />
+        </form>
+        <div className="col-lg-3 align-self-center" id="icons">
+          <i className="bi bi-facebook social-icons" />
+          <i className="bi bi-instagram social-icons" />
+          <a href="#Cart">
+            GIỎ HÀNG
+            <i className="bi bi-cart3 social-icons" />
+          </a>
         </div>
-    )
+      </div>
+      <div className="row bg-dark">
+        <nav>
+          <ul>
+            <li>
+              <a href="#Home">TRANG CHỦ</a>
+            </li>
+            <li>
+              <a href="#About">THÔNG TIN</a>
+            </li>
+            <li>
+              <a href="#Login">THIẾT KẾ</a>
+            </li>
+            <li>
+              <a href="#Collection">BỘ SƯU TẬP</a>
+            </li>
+            <li>
+              <a href="#Blog">BLOG</a>
+            </li>
+            <li>
+              <a href="#Login">ĐĂNG NHẬP</a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </div>
+  );
 }
-export default Navigator
+export default Navigator;
