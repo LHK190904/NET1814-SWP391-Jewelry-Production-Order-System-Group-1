009464368088ec32148805cc5385d597681e9f04
Nguyen Khoa <khoa060704@gmail.com>
2024-06-30 17:37:38 +0700

    convert xml to json v2

--- SWP/src/main/java/com/backendVn/SWP/services/XmlToJsonService.java
+++ SWP/src/main/java/com/backendVn/SWP/services/XmlToJsonService.java
@@ -1,8 +1,6 @@
 package com.backendVn.SWP.services;
-
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
@@ -13,15 +11,14 @@ public class XmlToJsonService {
     private final WebClient webClient;
     private final ObjectMapper objectMapper;
 
-    @Autowired
-    public XmlToJsonService(WebClient.Builder webClientBuilder) {
-        this.webClient = webClientBuilder.build();
+    public XmlToJsonService() {
+        this.webClient = WebClient.builder().baseUrl("http://api.btmc.vn").build();
         this.objectMapper = new ObjectMapper();
     }
 
     public Mono<JsonNode> getGoldPrices() {
         return webClient.get()
-                .uri("http://api.btmc.vn/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v")
+                .uri("/api/BTMCAPI/getpricebtmc?key=3kd8ub1llcg9t45hnoh8hmn7t5kc2v")
                 .retrieve()
                 .bodyToMono(String.class)
                 .map(this::convertStringToJsonNode);
