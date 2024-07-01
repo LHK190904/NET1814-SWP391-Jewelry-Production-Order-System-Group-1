f99508783701fa605a8f5f72471b3db94ded2b3b
Nguyen Khoa <khoa060704@gmail.com>
2024-07-01 11:14:28 +0700

    optimize create Request

--- SWP/src/main/java/com/backendVn/SWP/repositories/MaterialRepository.java
+++ SWP/src/main/java/com/backendVn/SWP/repositories/MaterialRepository.java
@@ -6,9 +6,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.time.Instant;
 import java.util.Date;
+import java.util.Optional;
 
 public interface MaterialRepository extends JpaRepository<Material, Integer> {
-    Material findByMaterialNameAndUpdateTime(String materialName, Instant updateTime);
+    Optional<Material> findByMaterialNameAndUpdateTime(String materialName, Instant updateTime);
 
     Material findByMaterialName(String materialName);
 }
