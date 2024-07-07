2d42f16759393d7b0ed5e7ce06a18478e449e2d2
trinhlong183 <trinhlong1804@gmail.com>
2024-07-07 23:23:17 +0700

    manage materials and product staff

--- src/pages/production-staff/process-orders/index.jsx
+++ src/pages/production-staff/process-orders/index.jsx
@@ -1,139 +1,29 @@
 import { useEffect, useState } from "react";
 import authService from "../../../services/authService";
 import axiosInstance from "../../../services/axiosInstance";
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
 import LogoutButton from "../../../components/logoutButton";
+import { Modal, Image } from "antd";
 
 function ProductionStaff() {
-  // const location = useLocation();
-  const [dataSource, setDataSource] = useState([
-    // {
-    //   orderID: "O1",
-    //   customerID: "C1",
-    //   description: "Description1",
-    //   design: "Design1",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O2",
-    //   customerID: "C2",
-    //   description: "Description2",
-    //   design: "Design2",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O3",
-    //   customerID: "C3",
-    //   description: "Description3",
-    //   design: "Design3",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O4",
-    //   customerID: "C4",
-    //   description: "Description4",
-    //   design: "Design4",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O5",
-    //   customerID: "C5",
-    //   description: "Description5",
-    //   design: "Design5",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O6",
-    //   customerID: "C6",
-    //   description: "Description6",
-    //   design: "Design6",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O7",
-    //   customerID: "C7",
-    //   description: "Description7",
-    //   design: "Design7",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O8",
-    //   customerID: "C8",
-    //   description: "Description8",
-    //   design: "Design8",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O9",
-    //   customerID: "C9",
-    //   description: "Description9",
-    //   design: "Design9",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O10",
-    //   customerID: "C10",
-    //   description: "Description10",
-    //   design: "Design10",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O11",
-    //   customerID: "C11",
-    //   description: "Description11",
-    //   design: "Design11",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O12",
-    //   customerID: "C12",
-    //   description: "Description12",
-    //   design: "Design12",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O13",
-    //   customerID: "C13",
-    //   description: "Description13",
-    //   design: "Design13",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O13",
-    //   customerID: "C13",
-    //   description: "Description13",
-    //   design: "Design13",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O14",
-    //   customerID: "C14",
-    //   description: "Description14",
-    //   design: "Design14",
-    //   status: "100%",
-    // },
-    // {
-    //   orderID: "O15",
-    //   customerID: "C15",
-    //   description: "Description15",
-    //   design: "Design15",
-    //   status: "100%",
-    // },
-  ]);
-
-  const [selectedOrder, setSelectedOrder] = useState([]);
+  const location = useLocation();
+  const [dataSource, setDataSource] = useState([]);
+  const [orderId, setOrderId] = useState("");
+  const [selectedOrder, setSelectedOrder] = useState(null);
   const [currentPage, setCurrentPage] = useState(1);
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [statusProcess, setStatusProcess] = useState("");
+  const [user, setUser] = useState(null);
   const itemsPerPage = 12;
 
   useEffect(() => {
     const fetchSalerData = async () => {
-      const user = authService.getCurrentUser();
-      console.log(user);
-      if (user && user.id) {
-        // setSaleName(user.username);
+      const currentUser = authService.getCurrentUser();
+      if (currentUser && currentUser.id) {
         try {
+          setUser(currentUser);
           const response = await axiosInstance.get(
-            `/request-orders/getAllOrderByProductionStaff/${user.id}`
+            `/request-orders/getAllOrderByProductionStaff/${currentUser.id}`
           );
           setDataSource(response.data.result || []);
         } catch (error) {
@@ -146,13 +36,43 @@ function ProductionStaff() {
     fetchSalerData();
   }, []);
 
-  const handleSelectOrder = (id) => {
-    const order = dataSource.find((order) => order.orderID === id);
-    setSelectedOrder(order ? [order] : []);
+  const handleSelectOrder = async (id) => {
+    try {
+      const response = await axiosInstance.get(`design/${id}`);
+      setSelectedOrder(response.data.result || null);
+      setOrderId(id);
+      const statusResponse = await axiosInstance.get(
+        `/process/getProcessByRequestOrderId/${id}`
+      );
+      setStatusProcess(statusResponse.data.result || {});
+    } catch (error) {
+      console.error("Không thể lấy chi tiết đơn hàng:", error);
+    }
   };
 
   const handlePageChange = (pageNumber) => {
     setCurrentPage(pageNumber);
+  };
+
+  const showModal = () => {
+    setIsModalVisible(true);
+  };
+
+  const handleCancel = () => {
+    setIsModalVisible(false);
+  };
+
+  const updateStatusProcess = async (orderId) => {
+    try {
+      await axiosInstance.post(`/process/${orderId}/${user.id}`);
+      // Fetch updated status process
+      const statusResponse = await axiosInstance.get(
+        `/process/getProcessByRequestOrderId/${orderId}`
+      );
+      setStatusProcess(statusResponse.data.result || {});
+    } catch (error) {
+      console.error("Không thể cập nhật trạng thái:", error);
+    }
   };
 
   const paginatedData = dataSource.slice(
@@ -190,34 +110,95 @@ function ProductionStaff() {
       </div>
 
       <div className="bg-[#434343] min-h-screen w-screen flex">
-        <div className="grid grid-cols-12 w-full ">
+        <div className="grid grid-cols-12 w-full">
           <div className="col-start-2 col-span-6 m-4 rounded-lg p-4 bg-gray-300 flex flex-col">
             <h1 className="bg-gray-400 p-4 text-2xl text-center">
               CHI TIẾT ĐƠN HÀNG
             </h1>
-            {selectedOrder.map((item) => (
-              <div
-                className="grid grid-cols-2 text-center flex-grow"
-                key={item.orderID}
-              >
-                <div className="col-span-2 border ">BẢN THIẾT KẾ</div>
-                <div className="col-span-2 border bg-white">
-                  {item.design}
-                  <img
-                    src="../src/assets/images/about1.jpg"
-                    alt=""
-                    className="w-[500px] h-[500px] mx-auto"
-                  />
+            {selectedOrder && (
+              <div className="space-y-4">
+                <div className="space-y-2">
+                  <div className="font-bold">BẢN THIẾT KẾ</div>
+                  <button
+                    className="bg-blue-500 text-white p-2 rounded-md"
+                    onClick={showModal}
+                  >
+                    Show images
+                  </button>
+                  <Modal
+                    title="Design Images"
+                    visible={isModalVisible}
+                    onCancel={handleCancel}
+                    footer={null}
+                  >
+                    <div className="flex flex-wrap justify-center">
+                      {selectedOrder.listURLImage.map((url, index) => (
+                        <Image
+                          key={index}
+                          src={url}
+                          alt={`Design ${index + 1}`}
+                          className="w-[200px] h-[200px] mx-2 my-2"
+                        />
+                      ))}
+                    </div>
+                  </Modal>
+                </div>
+                <div className="space-y-2">
+                  <div className="font-bold">MÔ TẢ</div>
+                  <div className="bg-white p-2 rounded-md">
+                    {selectedOrder.description}
+                  </div>
                 </div>
 
-                <div className="col-span-2 border">MÔ TẢ</div>
-                <div className="col-span-2 border bg-white">
-                  {item.description}
+                <div className="space-y-2">
+                  <div className="font-bold">CATEGORY</div>
+                  <div className="bg-white p-2 rounded-md">
+                    {selectedOrder.category}
+                  </div>
                 </div>
-                <div className="col-span-2 border">TRẠNG THÁI</div>
-                <div className="col-span-2 border bg-white">{item.status}</div>
+                <div className="space-y-2 flex gap-2">
+                  <div className="flex-1 space-y-2">
+                    <div className="font-bold mt-2">MAIN STONE ID</div>
+                    <div className="bg-white p-2 rounded-md w-full">
+                      {selectedOrder.mainStoneId || 0}
+                    </div>
+                  </div>
+                  <div className="flex-1 space-y-2">
+                    <div className="font-bold">SUB STONE ID</div>
+                    <div className="bg-white p-2 rounded-md w-full">
+                      {selectedOrder.subStoneId || 0}
+                    </div>
+                  </div>
+                </div>
+
+                <div className="space-y-2">
+                  <div className="font-bold">MATERIAL WEIGHT</div>
+                  <div className="bg-white p-2 rounded-md">
+                    {selectedOrder.materialWeight}
+                  </div>
+                </div>
+                <div className="space-y-2">
+                  <div className="font-bold">MATERIAL NAME</div>
+                  <div className="bg-white p-2 rounded-md">
+                    {selectedOrder.materialName}
+                  </div>
+                </div>
+                <div className="space-y-2">
+                  <div className="font-bold">Status of process</div>
+                  <div className="bg-white p-2 rounded-md">
+                    {statusProcess.status}
+                  </div>
+                </div>
+                <div className="space-y-2">
+                  <button
+                    className="bg-green-500 text-white p-2 rounded-md"
+                    onClick={() => updateStatusProcess(orderId)}
+                  >
+                    Update Status
+                  </button>
+                </div>
               </div>
-            ))}
+            )}
           </div>
           <div className="col-start-8 col-span-4 m-4 rounded-lg p-4 bg-gray-300 flex flex-col">
             <h1 className="text-center bg-gray-400 p-4 text-2xl">ĐƠN HÀNG</h1>
@@ -229,15 +210,15 @@ function ProductionStaff() {
               {paginatedData.map((order) => (
                 <div
                   className="grid grid-cols-2 text-center border"
-                  key={order.orderID}
+                  key={order.id}
                 >
                   <div className="col-span-1 p-2 bg-white border">
-                    <button onClick={() => handleSelectOrder(order.orderID)}>
-                      {order.orderID}
+                    <button onClick={() => handleSelectOrder(order.id)}>
+                      {order.id}
                     </button>
                   </div>
                   <div className="col-span-1 p-2 bg-white border">
-                    <button onClick={() => handleSelectOrder(order.orderID)}>
+                    <button onClick={() => handleSelectOrder(order.id)}>
                       {order.status}
                     </button>
                   </div>
@@ -247,7 +228,6 @@ function ProductionStaff() {
             <div className="flex justify-center mt-4">
               {Array.from({ length: totalPages }, (_, index) => (
                 <button
-                  // index start from 0
                   key={index + 1}
                   onClick={() => handlePageChange(index + 1)}
                   className={`mx-1 px-2 py-1 border ${
