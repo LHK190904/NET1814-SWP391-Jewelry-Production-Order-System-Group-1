79176390756b5228419a5301b9db5be737a99cc1
trinhlong183 <trinhlong1804@gmail.com>
2024-07-08 09:45:37 +0700

    config fix

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -2,15 +2,6 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
 
-// const firebaseConfig = {
-//   apiKey: "AIzaSyAMReUPanJVDBbQ6U1-5GdyoazFH3Xee6o",
-//   authDomain: "jewelry-production.firebaseapp.com",
-//   projectId: "jewelry-production",
-//   storageBucket: "jewelry-production.appspot.com",
-//   messagingSenderId: "1075441933073",
-//   appId: "1:1075441933073:web:ec2d529a0e15d749eecab0",
-//   measurementId: "G-E1YN8Z8DQ2",
-// };
 
 const firebaseConfig = {
   apiKey: "AIzaSyDPbts0vYP7J0yLvxyLse7cAj23AXxMN2A",
