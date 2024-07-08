fe06bfedcbb45199256a913f311834fdf6475144
LeNguyenChiBao <baolncse183721@fpt.edu.vn>
2024-07-08 21:29:54 +0700

    mmb

--- src/pages/cart/order/index.jsx
+++ src/pages/cart/order/index.jsx
@@ -12,57 +12,75 @@ function CartOrder() {
   const [selectedImage, setSelectedImage] = useState(null);
   const [feedback, setFeedback] = useState("");
 
-  const fetchOrders = async () => {
-    if (!requestID) {
+  useEffect(() => {
+    if (requestID) {
+      fetchOrders();
+    } else {
       console.error("Request ID is undefined");
-      return;
     }
+  }, [requestID]);
+
+  useEffect(() => {
+    if (process && process.status === "100%") {
+      handleCreateInvoice();
+    }
+  }, [process]);
+
+  const fetchOrders = async () => {
     try {
-      // Get order info
       const responseOrder = await axiosInstance.get(
         `request-orders/getOrderByRequestIdForCustomer/${requestID}`
       );
-      console.log("Order data:", responseOrder.data.result);
       setOrder(responseOrder.data.result);
-      try {
-        // Get design image
-        const responseDesign = await axiosInstance.get(
-          `design/${responseOrder.data.result.id}`
-        );
-        setDesign(responseDesign.data.result);
-        console.log("Design:", responseDesign.data.result);
-      } catch (error) {
-        console.log(`No image found`);
-      }
-      try {
-        const responseProcess = await axiosInstance.get(
-          `process/getProcessByRequestOrderId/${responseOrder.data.result.id}`
-        );
-        setProcess(responseProcess.data.result);
-        console.log("Process", responseProcess.data.result);
-      } catch (error) {
-        console.log(error);
-      }
+      console.log(responseOrder.data.result);
+      const responseDesign = await axiosInstance.get(
+        `design/${responseOrder.data.result.id}`
+      );
+      setDesign(responseDesign.data.result);
+      console.log(responseDesign.data.result);
+      const responseProcess = await axiosInstance.get(
+        `process/getProcessByRequestOrderId/${responseOrder.data.result.id}`
+      );
+      setProcess(responseProcess.data.result);
     } catch (error) {
       console.error("Error fetching orders:", error);
       setOrder({});
     }
   };
 
-  useEffect(() => {
-    fetchOrders();
-  }, [requestID]);
+  const handleCreateInvoice = async () => {
+    try {
+      const response1 = await axiosInstance.post(`invoices/${order.requestID}`);
+      console.log(response1.data.result);
+      if (response1.data.result) {
+        const response2 = await axiosInstance.post(
+          `invoice-details/${order.id}`
+        );
+        console.log(response2.data.result);
+      }
+    } catch (error) {
+      if (error.response) {
+        console.error("Error response:", error.response.data);
+        console.error(`Error: ${error.response.data.message}`);
+      } else if (error.request) {
+        console.error("Error request:", error.request);
+        console.error("No response from server");
+      } else {
+        console.error("Error", error.message);
+        console.error("Error creating invoice");
+      }
+    }
+  };
 
   const handleApprove = async () => {
     try {
       const values = { feedback };
-      console.log("Approve values:", values); // Log the values
-      const response = await axiosInstance.put(
+      await axiosInstance.put(
         `request-orders/acceptDesign/${design.id}`,
         values
       );
-      message.success(`Đã gửi`);
-      setFeedback(""); // Reset feedback field
+      message.success("Đã gửi");
+      setFeedback("");
     } catch (error) {
       console.log(error);
       message.error("Có lỗi xảy ra");
@@ -71,17 +89,10 @@ function CartOrder() {
 
   const handleDeny = async () => {
     try {
-      const values = { feedback };
-      console.log("Deny values:", values);
-      const requestData = {
-        description: feedback,
-      };
-      const response = await axiosInstance.put(
-        `design/denyDesign/${design.id}`,
-        requestData
-      );
-      message.success(`Đã gửi`);
-      setFeedback(""); // Reset feedback field
+      const requestData = { description: feedback };
+      await axiosInstance.put(`design/denyDesign/${design.id}`, requestData);
+      message.success("Đã gửi");
+      setFeedback("");
     } catch (error) {
       console.log(error);
       message.error("Có lỗi xảy ra");
@@ -96,7 +107,6 @@ function CartOrder() {
     setFeedback(e.target.value);
   };
 
-  // Determine the current step based on the process status
   const getCurrentStep = () => {
     if (process && process.status) {
       switch (process.status) {
@@ -129,102 +139,80 @@ function CartOrder() {
                 <div className="my-4 text-2xl text-center">
                   Đang chờ phân công công việc . . .
                 </div>
-              ) : order.status === "Assigned" ||
-                order.status === "Design Denied" ||
-                order.status === "Waiting for customer's decision" ? (
-                <div className="grid grid-cols-12 gap-4">
-                  {design.listURLImage && design.listURLImage.length > 0 ? (
-                    <div className="col-span-6 flex flex-col items-center">
-                      <img
-                        src={selectedImage || design.listURLImage[0]}
-                        alt="Chưa có hình ảnh"
-                        className="rounded-lg w-[300px] h-[300px]"
-                      />
-                      <div className="flex flex-wrap justify-center mt-4">
-                        {design.listURLImage.map((url, index) => (
-                          <img
-                            key={index}
-                            src={url}
-                            alt="Chưa có hình ảnh"
-                            className="rounded-lg cursor-pointer w-[100px] h-[100px] mx-1"
-                            onClick={() => handleImageClick(url)}
-                          />
-                        ))}
-                      </div>
-                    </div>
-                  ) : (
-                    <div className="col-span-12 text-center">
-                      Chưa có hình ảnh
-                    </div>
-                  )}
-                  <div className="col-span-6 text-xl">
-                    <div className="flex flex-col justify-evenly h-full p-1">
-                      <form>
-                        <label className="text-2xl">Feedback</label>
-                        <textarea
-                          value={feedback}
-                          onChange={handleChange}
-                          required
-                          className="w-full p-2 border border-gray-300 rounded"
-                        />
-                        <div className="flex justify-around mt-4">
-                          <Button
-                            className="w-full bg-green-400 p-2 font-bold"
-                            onClick={handleApprove}
-                          >
-                            Approve
-                          </Button>
-                          <Button
-                            className="w-full bg-red-400 p-2 font-bold"
-                            onClick={handleDeny}
-                          >
-                            Deny
-                          </Button>
-                        </div>
-                      </form>
-                    </div>
-                  </div>
-                </div>
               ) : (
                 <div className="grid grid-cols-12 gap-4">
-                  {design.listURLImage && design.listURLImage.length > 0 ? (
-                    <div className="col-span-6 flex flex-col items-center">
-                      <img
-                        src={selectedImage || design.listURLImage[0]}
-                        alt=""
-                        className="rounded-lg w-[300px] h-[300px]"
-                      />
-                      <div className="flex flex-wrap justify-center mt-4">
-                        {design.listURLImage.map((url, index) => (
-                          <img
-                            key={index}
-                            src={url}
-                            alt=""
-                            className="rounded-lg cursor-pointer w-[100px] h-[100px] mx-1"
-                            onClick={() => handleImageClick(url)}
-                          />
-                        ))}
+                  <div className="col-span-6 flex flex-col items-center">
+                    {design.listURLImage && design.listURLImage.length > 0 ? (
+                      <>
+                        <img
+                          src={selectedImage || design.listURLImage[0]}
+                          alt="Chưa có hình ảnh"
+                          className="rounded-lg w-[300px] h-[300px]"
+                        />
+                        <div className="flex flex-wrap justify-center mt-4">
+                          {design.listURLImage.map((url, index) => (
+                            <img
+                              key={index}
+                              src={url}
+                              alt="Chưa có hình ảnh"
+                              className="rounded-lg cursor-pointer w-[100px] h-[100px] mx-1"
+                              onClick={() => handleImageClick(url)}
+                            />
+                          ))}
+                        </div>
+                      </>
+                    ) : (
+                      <div className="col-span-12 text-center">
+                        Chưa có hình ảnh
                       </div>
-                    </div>
-                  ) : (
-                    <div className="col-span-12 text-center">
-                      Chưa có hình ảnh
-                    </div>
-                  )}
+                    )}
+                  </div>
                   <div className="col-span-6 text-xl">
-                    <div className="flex flex-col justify-evenly h-full">
-                      <div>LOẠI TRANG SỨC: {design.category}</div>
-                      <div>LOẠI VÀNG: {design.materialName}</div>
-                      <div>TRỌNG LƯỢNG: {design.materialWeight}</div>
-                      <div>ĐÁ CHÍNH: {design.mainStoneId}</div>
-                      <div>ĐÁ PHỤ: {design.subStoneId}</div>
-                      <div>MÔ TẢ: {design.description}</div>
-                      <div>TIẾN TRÌNH: {process ? process.status : "N/A"}</div>
-                      <Stepper currentStep={getCurrentStep()} />
-                      <div>
-                        CẬP NHẬT LÚC: {process ? process.updatedAt : "N/A"}
+                    {order.status === "Assigned" ||
+                    order.status === "Design Denied" ||
+                    order.status === "Waiting for customer's decision" ? (
+                      <div className="flex flex-col justify-evenly h-full p-1">
+                        <form>
+                          <label className="text-2xl">Feedback</label>
+                          <textarea
+                            value={feedback}
+                            onChange={handleChange}
+                            required
+                            className="w-full p-2 border border-gray-300 rounded"
+                          />
+                          <div className="flex justify-around mt-4">
+                            <Button
+                              className="w-full bg-green-400 p-2 font-bold"
+                              onClick={handleApprove}
+                            >
+                              Approve
+                            </Button>
+                            <Button
+                              className="w-full bg-red-400 p-2 font-bold"
+                              onClick={handleDeny}
+                            >
+                              Deny
+                            </Button>
+                          </div>
+                        </form>
                       </div>
-                    </div>
+                    ) : (
+                      <div className="flex flex-col justify-evenly h-full">
+                        <div>LOẠI TRANG SỨC: {design.category}</div>
+                        <div>LOẠI VÀNG: {design.materialName}</div>
+                        <div>TRỌNG LƯỢNG: {design.materialWeight}</div>
+                        <div>ĐÁ CHÍNH: {design.mainStoneId}</div>
+                        <div>ĐÁ PHỤ: {design.subStoneId}</div>
+                        <div>MÔ TẢ: {design.description}</div>
+                        <div>
+                          TIẾN TRÌNH: {process ? process.status : "N/A"}
+                        </div>
+                        <Stepper currentStep={getCurrentStep()} />
+                        <div>
+                          CẬP NHẬT LÚC: {process ? process.updatedAt : "N/A"}
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               )}
