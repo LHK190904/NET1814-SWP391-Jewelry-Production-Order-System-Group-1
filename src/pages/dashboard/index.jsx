54eabdea2bfdeb452217c9bbbef313ae0f785685
LeNguyenChiBao <baolncse183721@fpt.edu.vn>
2024-07-23 00:36:28 +0700

    Dashboard updated

--- src/pages/dashboard/index.jsx
+++ src/pages/dashboard/index.jsx
@@ -1,26 +1,16 @@
 import LatestTransaction from "./transactions";
-import SaleByCategory from "./category";
 import TopSelling from "./top-selling";
 import SaleTrend from "./trend";
 
 function Dashboard() {
   return (
     <div className="w-screen min-h-screen bg-gray-100">
-      <div className="grid grid-cols-12 gap-4 m-4">
-        <div className="col-start-2 col-span-6">
-          <SaleTrend />
-        </div>
-        <div className="col-start-8 col-span-4">
-          <LatestTransaction />
-        </div>
+      <div className="w-3/4 mx-auto">
+        <SaleTrend />
       </div>
-      <div className="grid grid-cols-12 gap-4">
-        <div className="col-start-2 col-span-5">
-          <SaleByCategory />
-        </div>
-        <div className="col-start-7 col-span-5 ">
-          <TopSelling />
-        </div>
+      <div className="flex justify-evenly m-4 p-4">
+        <TopSelling />
+        <LatestTransaction />
       </div>
     </div>
   );
--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -102,6 +102,7 @@ function Home() {
         throw new Error("Vui lòng đăng nhập để đặt yêu cầu");
       }
       await axiosInstance.post(`/requests/${user.id}`, formData);
+
       handleHideModal();
       message.success("Đặt yêu cầu thành công");
       form.resetFields();
