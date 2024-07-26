77eed70a93849dd8675c0c4b7aac7cbf8c80fa37
LHK190904 <hoangkhanh19092004@gmail.com>
2024-07-26 22:25:17 +0700

    update payment

--- SWP/src/main/java/com/backendVn/SWP/controllers/PaymentController.java
+++ SWP/src/main/java/com/backendVn/SWP/controllers/PaymentController.java
@@ -2,14 +2,12 @@ package com.backendVn.SWP.controllers;
 
 import com.backendVn.SWP.dtos.response.ApiResponse;
 import com.backendVn.SWP.dtos.response.PaymentResponse;
+import com.backendVn.SWP.entities.Payment;
 import com.backendVn.SWP.services.PaymentService;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/payment")
@@ -20,9 +18,17 @@ public class PaymentController {
     PaymentService paymentService;
 
     @PostMapping("/{requestId}")
-    public ApiResponse<PaymentResponse> createPayment(@PathVariable Integer requestId) {
+    public ApiResponse<PaymentResponse> createDeposit(@PathVariable Integer requestId) {
         return ApiResponse.<PaymentResponse>builder()
                 .result(paymentService.createDeposit(requestId))
                 .build();
     }
+
+    @PostMapping("/createPayment/{requestId}")
+    public ApiResponse<PaymentResponse> createPayment(@PathVariable Integer requestId) {
+        return ApiResponse.<PaymentResponse>builder()
+                .result(paymentService.createPayment(requestId))
+                .build();
+    }
+    
 }
